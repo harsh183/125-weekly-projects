7dd8feb673ae8a71f9a6d04ea5604336eda67b56
Harsh Deep <hrshdeep51@gmail.com>
2021-05-18 21:13:16 -0500

    add files

--- code/fancy_cli/fgit.py
+++ code/fancy_cli/fgit.py
@@ -1,11 +1,8 @@
-# from __future__ import print_function, unicode_literals
-
 from pyfiglet import Figlet
 from plumbum import cli
-from plumbum.cmd import ls
-# from plumbum.cmd import ls, git
-# from PyInquirer import prompt, print_json
-#from pprint import pprint
+from plumbum.cmd import ls, git
+
+from PyInquirer import prompt
 
 def print_banner(text):
     print(Figlet(font='slant').renderText(text))
@@ -15,17 +12,28 @@ def get_files():
     files = ls_output.split("\n")
     return files
 
-# def generate_questions(files, commit_flag):
+def generate_questions(files):
+    questions = [{
+        'type': 'checkbox',
+        'name': 'files',
+        'message': 'What would you like to commit',
+        'choices': [{'name': file.strip()} for file in files],   
+    }]
+    return questions
 
 class FancyGitAdd(cli.Application):
     VERSION = "1.3"
-    # commit = cli.Flag(['c', 'commit'], help="Commits the added files as well")
+    commit = cli.Flag(['c', 'commit'], help="Commits the added files as well")
 
     def main(self):
         print_banner("Git fancy add")
         files = get_files()
-        # if (self.commit):
-            # print("I see you also want to commit")
+
+        questions = generate_questions(files)
+        answers = prompt(questions)
+        git('add', answers['files'])
+        if (self.commit):
+            git('commit', '-m', 'add files')
 
 if __name__ == "__main__":
     FancyGitAdd()
@@ -35,6 +43,15 @@ if __name__ == "__main__":
 def test_get_files():
     files = get_files()
     assert len(files) == 5
+
+def test_generate_questions():
+    files = ["best.rb", "good.kt", "small.py"]
+    questions = generate_questions(files)
+    assert len(questions) == 1 # one question
+
+    question = questions[0]
+    assert question['type'] == 'checkbox' # has to be a checkbox
+    assert len(question['choices']) == len(files) # same size as files
 
 
 # fancy_ascii("Fancy Git Add")
